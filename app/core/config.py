ce04a8a558d86411b2f79766822c198565861e0a
samal <samalbahrii@icloud.com>
2026-03-26 21:25:00 -0400

    set up the config and .env

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str = "HS256"
+
 
 
 settings = Settings()
